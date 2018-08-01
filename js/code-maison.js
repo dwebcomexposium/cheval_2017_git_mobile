f059020c107cc536e14c1372381b55abfb553988
ThomasRny <33687666+ThomasRny@users.noreply.github.com>
2018-08-01 12:23:40 +0200

    Initialized cheval Mobile from kloudspeaker

---
+++ js/code-maison.js
@@ -0,0 +1,32 @@
+/* $(document).ready(function(){
+
+  $('.front .list-grids-with-pagin .grid-la-list').slick({
+    infinite: true,
+   track: function() { return $(this).children('grid-la-list'); },
+    slidesToScroll: 3,
+    centerMode: true,
+    centerPadding: '0px',
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '0px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '0px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
+}); */
